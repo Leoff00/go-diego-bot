fe459b534dc7910abe3cfe4cb8744bbd236d637b
leoff00 <leof0180@gmail.com>
2023-02-13 12:05:12 -0300

    feat: including new handler fn to struct

--- handlers/handler_struct.go
+++ handlers/handler_struct.go
@@ -4,6 +4,7 @@ import "github.com/bwmarrin/discordgo"
 
 type HandlersProps struct {
 	msgPingPongHanlder func(s *discordgo.Session, m *discordgo.MessageCreate)
+	ImgGenerator       func(s *discordgo.Session, m *discordgo.MessageCreate)
 	helpJavaHandler    func(s *discordgo.Session, m *discordgo.MessageCreate)
 	msgGreeting        func(s *discordgo.Session, m *discordgo.MessageCreate)
 	notifyNewMember    func(s *discordgo.Session, g *discordgo.GuildMemberAdd)
