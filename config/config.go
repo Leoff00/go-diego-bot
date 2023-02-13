defbfd1ca1f702b0b771764468590f535176ee05
leoff00 <leof0180@gmail.com>
2023-02-13 20:11:51 -0300

    style: ident files and remove useless fn

--- config/config.go
+++ config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 
 	"github.com/Leoff00/go-diego-bot/envs"
@@ -24,14 +23,12 @@ func ReadConfig() error {
 	file, err := os.ReadFile("./config.json")
 
 	if err != nil {
-		fmt.Println((err.Error()))
 		return err
 	}
 
 	err = json.Unmarshal(file, &config)
 
 	if err != nil {
-		fmt.Println(err.Error())
 		return err
 	}
 
